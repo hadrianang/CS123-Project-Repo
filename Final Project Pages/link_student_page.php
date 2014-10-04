35eed4a1aebbb9da0985c8d5067f93bd5158fcee
Hadrian <hadrianang@outlook.com>
2014-10-05 01:59:57 +0800

    Edited link_student_page to show current mentor before linking

--- Final Project Pages/link_student_page.php
+++ Final Project Pages/link_student_page.php
@@ -13,11 +13,24 @@
 	$quer = "SELECT * FROM account WHERE Type = 'student' ORDER BY Name"; 
 	$results = mysqli_query($conn, $quer); 
 	//change link below when needed
+	echo 
+	"<table width = 50%>	
+		<tr>
+			<td><b>Student Name</b></td>
+			<td><b>Current Mentor</b></td>
+		</tr>";
 	while ($arr = mysqli_fetch_array($results))
 	{
 		$u = $arr['Username']; 
 		$n = $arr['Name']; 
-		echo "<a href=link_student.php?username=" . $u . "&name=" . $n ."> $n</a><br>";
+		$getMentor = mysqli_query($conn, "SELECT * FROM account_student WHERE Username = '$u'");
+		$temp = mysqli_fetch_array($getMentor); 
+		$m = $temp['Mentor']; 
+		echo "<tr>
+				<td><a href=link_student.php?username=" . $u . "&name=" . $n ."> $n</a> </td>
+				<td>$m <br></td> 
+			</tr>";
 	}
+	echo "</table>";
 ?>
 </html> 
