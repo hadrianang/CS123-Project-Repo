a549dd18adc1d48f86f5bd25ab5fe3fce7309487
Hadrian Ang <hadrianang@outlook.com>
2014-10-17 20:52:05 +0800

    Added legend for required fields

--- Final Project Pages/create_account_page.php
+++ Final Project Pages/create_account_page.php
@@ -1,4 +1,4 @@
-<html>
+<html><title>PracSys: Ateneo DISCS Practicum Management System</title>
 	<link rel="stylesheet" href="style.css">
 	<?php
 		include 'page_setup.php';
@@ -9,10 +9,10 @@
 		<center>
 		<h1> Account Creation Page </h1>
 			<form action = "create_account.php">
-				Username: <input type = "text" name = "username" value = ""><br>
-				Password: <input type = "password" name = "password" value = ""><br>
-				Name: <input type = "text" name = "name" value =""><br>
-				ID Number (if student or faculty): <input type = "text" name = "idnum" value =""> <br>
+				*Username: <input type = "text" name = "username" value = ""><br>
+				*Password: <input type = "password" name = "password" value = ""><br>
+				*Name: <input type = "text" name = "name" value =""><br>
+				-ID Number (if student or faculty): <input type = "text" name = "idnum" value =""> <br>
 				Course (if student): 
 				<select name = "course">
 					<option value="bs cs">BS CS</option>
@@ -38,15 +38,24 @@
 					$quer = "SELECT Name FROM Company"; 
 					$companies = mysqli_query($conn, $quer); 
 				?>
-				Company:
+				+Company:
 				<select name="company">
 					<?php while ($arr = mysqli_fetch_array($companies)): ?>
 						<option><?php echo $arr['Name']; ?></option>
 					<?php endwhile; ?>
 				</select><br>
-				Department: <input type = "text" name = "department" value = ""><br>
-				Title: <input type = "text" name = "title" value = ""><br><br>
-				<input type = "submit" name = "submit" value = "Create Account">
+				+Department: <input type = "text" name = "department" value = ""><br>
+				+Title: <input type = "text" name = "title" value = ""><br><br>
+				<input type="submit" value="Create Account" onclick="this.disabled=true;this.value='Sending, please wait...';this.form.submit();"></input>
+				<br>
+				<br>
+				<font size = 2>
+				Legend:<br>
+				* required for all accounts <br>
+				+ required for mentor accounts<br>
+				- required for student or faculty accounts<br>
+				</font>
+				
 			</form> 
 		</center>
 	</div>
